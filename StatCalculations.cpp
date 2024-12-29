71fb7b3498a4a42609ef7ad067870455b3631baf
M-Amir-Ash <145683856+M-Amir-Ash@users.noreply.github.com>
2024-12-29 12:40:10 +0700

    Update StatCalculations.cpp

--- StatCalculations.cpp
+++ StatCalculations.cpp
@@ -2,15 +2,20 @@
 using namespace std;
 
 // Function to calculate stats recursively for a single level
-void calculateStatsRecursive(int level, float baseHP, float baseATK, float baseDEF, float baseSpeed, float growthHP, float growthATK, float growthDEF, int& hp, int& atk, int& def) {
-    if (level == 1) {
-        hp = (int)(baseHP + 0.5);
-        atk = (int)(baseATK + 0.5);
-        def = (int)(baseDEF + 0.5);
+void calculateStatsRecursive(int levels[], float baseHP, float baseATK, float baseDEF, float baseSpeed, float growthHP, float growthATK, float growthDEF, int& hp, int& atk, int& def ,int size, int& sizeFirst) {
+    int level = levels[sizeFirst];
+    if (level == 80) {
+        hp = (int)(baseHP + (level - 1) * growthHP + 0.5);
+        atk = (int)(baseATK + (level - 1) * growthATK + 0.5);
+        def = (int)(baseDEF + (level - 1) * growthDEF + 0.5);
+        cout << "Level: " << level << ", HP: " << hp << ", ATK: " << atk << ", DEF: " << def << endl;
     } else {
         hp = (int)(baseHP + (level - 1) * growthHP + 0.5);
         atk = (int)(baseATK + (level - 1) * growthATK + 0.5);
         def = (int)(baseDEF + (level - 1) * growthDEF + 0.5);
+        cout << "Level: " << level << ", HP: " << hp << ", ATK: " << atk << ", DEF: " << def << endl;
+        sizeFirst++;
+        calculateStatsRecursive(levels, baseHP, baseATK, baseDEF, 0, growthHP, growthATK, growthDEF, hp, atk, def, size, sizeFirst);
     }
 }
 
@@ -30,13 +35,10 @@ int main() {
     float growthHP = 8.91, growthATK = 5.72, growthDEF = 8.49;
     int levels[] = {1, 20, 30, 40, 50, 60, 70, 80};
     int size = 8;
-
+    int sizeFirst = 0;
     cout << "Recursive Calculation:" << endl;
-    for (int i = 0; i < size; i++) {
-        int hp, atk, def;
-        calculateStatsRecursive(levels[i], baseHP, baseATK, baseDEF, 0, growthHP, growthATK, growthDEF, hp, atk, def);
-        cout << "Level: " << levels[i] << ", HP: " << hp << ", ATK: " << atk << ", DEF: " << def << endl;
-    }
+    int hp, atk, def;
+    calculateStatsRecursive(levels, baseHP, baseATK, baseDEF, 0, growthHP, growthATK, growthDEF, hp, atk, def, size, sizeFirst);
 
     cout << "\nIterative Calculation:" << endl;
     calculateStatsIterative(baseHP, baseATK, baseDEF, 0, growthHP, growthATK, growthDEF, levels, size);
